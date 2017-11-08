91561b7fdb5c90e01f5533a9e3905483e44e3c6d
Colin Walsh <colinfwalsh@gmail.com>
2017-11-08 14:31:55 -0500

    Cleaned up code, removed terrible tutorial code

--- loginTestApp/ViewController.swift
+++ loginTestApp/ViewController.swift
@@ -15,23 +15,25 @@ import FBSDKLoginKit
 class ViewController: UIViewController, LoginButtonDelegate {
     
     var dict: [String : AnyObject]!
-    let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+    var loginButton: LoginButton {
+        let login = LoginButton(readPermissions: [ .publicProfile ])
+        login.center = view.center
+        view.addSubview(login)
+        return login
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loginButton.delegate = self
         
-        //creating button
-      
-        
-        
         if let _ = FBSDKAccessToken.current(){
             getFBUserData()
         }
+        
         //adding it to view
-        loginButton.center = view.center
-        view.addSubview(loginButton)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -48,7 +50,7 @@ class ViewController: UIViewController, LoginButtonDelegate {
     }
     
     func loginButtonDidLogOut(_ loginButton: LoginButton) {
-        print("logged out")
+        print("Sorry to see you go! You are logged out.")
     }
     
     //function is fetching the user data
@@ -56,32 +58,47 @@ class ViewController: UIViewController, LoginButtonDelegate {
         if (FBSDKAccessToken.current() != nil) {
             FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) in
                 
+                // If we get an error, print it and then break out of the function
                 if let error = error {
                     print(error)
                     return
                 }
                 
+                // Check to make sure result is not nil, if it is break out
                 guard let resultData = result else {return}
                 
+                // Print it to see what we get back
                 print(resultData)
                 
+                // Attempt to cast the result as a dictionary of <Key: String, Value: AnyObject> else break out of the function
                 guard let returnDict = resultData as? [String: AnyObject] else {return}
                 
+                // Last check to make sure the cast was successful
                 print(returnDict)
                 
+                // We set our property at the top to returnDict
                 self.dict = returnDict
                 
-                
-                
             })
-                /*.start(completionHandler: { (connection, result, error) -> Void in
+            
+            /*
+             
+             Original tutorial did this, DO NOT HANDLE DATA THIS WAY
+                .start(completionHandler: { (connection, result, error) -> Void in
                 
-               
+                The thinking here was to check to see if the error existed, if it did not
+                then they went ahead and did the forced cast of the result to a dictionary
+                set it to our property at the top.  This can fail in a number of ways, and
+                is pretty dangerous.  While this probably will work 99% of the time (since we know that the data will largely
+                remain consistent from Facebook) it's better to have checks to make sure the app does not crash or have undefined
+                behavior.
+             
                 if (error == nil){
                     self.dict = result as! [String : AnyObject]
                     print(result!)
                     print(self.dict)
                 }
+             
             })*/
         }
     }
